8f457920d1f5ef75f889985e9fab0dd168efad0d
Long <nguyenxuanphilong@gmail.com>
2026-04-22 11:32:10 +0700

    message constants update

--- Backend/src/main/java/com/luvina/la/config/Constants.java
+++ Backend/src/main/java/com/luvina/la/config/Constants.java
@@ -1,7 +1,7 @@
 /*
  * Copyright(C) 2010 Luvina Software Company
  *
- * Constants.java, April 17, 2026 nxplong
+ * Constants.java, April 22, 2026 nxplong
  */
 package com.luvina.la.config;
 
@@ -48,15 +48,35 @@ public class Constants {
 
         // Các mã định danh lỗi hệ thống
         public static final String CODE_ER001 = "ER001";
+        public static final String CODE_ER002 = "ER002";
+        public static final String CODE_ER003 = "ER003";
+        public static final String CODE_ER004 = "ER004";
+        public static final String CODE_ER005 = "ER005";
         public static final String CODE_ER006 = "ER006";
+        public static final String CODE_ER007 = "ER007";
+        public static final String CODE_ER008 = "ER008";
+        public static final String CODE_ER009 = "ER009";
+        public static final String CODE_ER010 = "ER010";
+        public static final String CODE_ER011 = "ER011";
+        public static final String CODE_ER012 = "ER012";
         public static final String CODE_ER013 = "ER013";
         public static final String CODE_ER014 = "ER014";
         public static final String CODE_ER015 = "ER015";
         public static final String CODE_ER016 = "ER016";
+        public static final String CODE_ER017 = "ER017";
         public static final String CODE_ER018 = "ER018";
+        public static final String CODE_ER019 = "ER019";
+        public static final String CODE_ER020 = "ER020";
         public static final String CODE_ER021 = "ER021";
         public static final String CODE_ER022 = "ER022";
         public static final String CODE_ER023 = "ER023";
+
+        // Các mã thông báo thành công
+        public static final String CODE_MSG001 = "MSG001";
+        public static final String CODE_MSG002 = "MSG002";
+        public static final String CODE_MSG003 = "MSG003";
+        public static final String CODE_MSG004 = "MSG004";
+        public static final String CODE_MSG005 = "MSG005";
 
         public static final int MAX_EMPLOYEE_NAME_LENGTH = 125;
         public static final String PARAM_EMPLOYEE_NAME = "氏名";
